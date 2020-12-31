682690391ec5dd34f522102c1733e722d2e0c4d6
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-12-31 20:06:27 +0900

    [from now] 2020/12/31 20:06:27
     setup.py                         |  2 ++
     websshproxy/__init__.py          | 55 +++++++++++++++++++++++++++++++------
     websshproxy/templates/index.html | 59 ++++++++++++++++++++++++++++++++++++----
     3 files changed, 101 insertions(+), 15 deletions(-)

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setup(
     install_requires=[
         'Flask',
         'Flask-SocketIO',
+        'Flask-Executor',
+        'paramiko',
     ],
 )
--- websshproxy/__init__.py
+++ websshproxy/__init__.py
@@ -1,34 +1,71 @@
 # coding=utf-8
+import collections
+
 from flask import Flask
+from flask import request
 from flask import render_template
 
 from flask_socketio import SocketIO
 
+from flask_executor import Executor
+
+import paramiko
+
+
 # flask
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+# socketio
 socketio = SocketIO(app)
+# executor
+executor = Executor()
+executor.init_app(app)
+
+
+input_buf = collections.defaultdict(str)
+
+
+def start_session(sid, hostname, port, username, password):
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=hostname, port=port, username=username, password=password, timeout=1)
+        channel = ssh.invoke_shell()
+        socketio.emit('ssh-status', 'connected', json=True, to=sid)
+        while True:
+            if input_buf[sid]:
+                channel.send(input_buf[sid].encode('utf-8'))
+                input_buf[sid] = ''
+            if channel.recv_ready():
+                ssh_output = channel.recv(65536).decode('utf-8')
+                socketio.emit('ssh-output', ssh_output, to=sid)
+            socketio.sleep(0.1)
+    except Exception as e:
+        socketio.emit('ssh-status', str(e), json=True, to=sid)
 
 
 @socketio.on('message')
 def handle_message(data):
     print('received message: ' + data)
 
+
 @socketio.on('json')
 def handle_json(json):
     print('received json: ' + str(json))
 
-@socketio.on('my event')
-def handle_my_custom_event(json):
-    print('received json: ' + str(json))
 
-@socketio.on('connect')
-def test_connect():
-    print('Client connected')
+@socketio.on('ssh-connect')
+def handle_ssh_connect(json):
+    try:
+        print("{:s}@{:s}".format(json['username'], json['hostname']))
+        executor.submit(start_session, request.sid, json['hostname'], json['port'], json['username'], json['password'])
+    except Exception as e:
+        socketio.emit('ssh-status', str(e), json=True, to=request.sid)
 
-@socketio.on('disconnect')
-def test_disconnect():
-    print('Client disconnected')
+
+@socketio.on('ssh-input')
+def handle_ssh_input(json):
+    input_buf[request.sid] += json
 
 
 @app.route('/', methods=['GET'])
